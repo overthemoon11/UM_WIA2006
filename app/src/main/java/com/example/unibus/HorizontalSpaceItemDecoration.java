a18a78d92862826f19083c7e8854a65501a1dfb7
overthemoon11 <shu.hui051603@gmail.com>
2023-12-18 02:26:53 +0800

    save

---
+++ app/src/main/java/com/example/unibus/HorizontalSpaceItemDecoration.java
@@ -0,0 +1,26 @@
+package com.example.unibus;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HorizontalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int spaceWidth;
+
+    public HorizontalSpaceItemDecoration(int spaceWidth) {
+        this.spaceWidth = spaceWidth;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+
+        // Apply spacing to the right of each item (except the last one)
+        if (parent.getChildAdapterPosition(view) < parent.getAdapter().getItemCount() - 1) {
+            outRect.right = spaceWidth;
+        }
+    }
+}
